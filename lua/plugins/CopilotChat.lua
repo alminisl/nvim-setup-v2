15db4ed8e96abcf8434fdb4184c155b26df724f6
Almin Islamović <almin.islamovic@gmail.com>
2024-09-04 11:24:41 +0200

    Fixed CopilotChat

--- lua/plugins/CopilotChat.lua
+++ lua/plugins/CopilotChat.lua
@@ -1,4 +1,14 @@
 return {
-
-  { "gptlang/CopilotChat.nvim" },
+ "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
 }
