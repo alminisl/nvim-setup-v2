d9dc3d23aa7a84e0ba7a9c9cf2e7a39a305af67b
Almin Islamović <almin.islamovic@gmail.com>
2024-09-05 11:04:53 +0200

    Updated something but feels bad

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,7 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "github_dark_default" } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { "catppuccin/nvim", name = "catppuccin-mocha", priority = 1000 } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
@@ -42,7 +42,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin-mocha" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
